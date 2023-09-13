4af3d08d66220ad24a784fe2105fcbd58b292739
hmArthur <82618569+hmArthur@users.noreply.github.com>
2023-09-13 08:14:55 -0300

    a

--- som.js
+++ som.js
@@ -2,3 +2,10 @@ function playAudio(url) {
     let beat = new Audio(url);
     beat.play();
 }
+
+document.addEventListener("keydown", function(e){
+	if (e.keyCode == 27){
+		e.preventDefault();
+		playAudio('keya.wav')
+	}
+}, false);
