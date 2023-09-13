5cdffe3dc7e75bce644b2dc6b987e269a194d395
hmArthur <82618569+hmArthur@users.noreply.github.com>
2023-09-13 08:19:39 -0300

    a

--- som.js
+++ som.js
@@ -4,8 +4,57 @@ function playAudio(url) {
 }
 
 document.addEventListener("keydown", function(e){
-	if (e.keyCode == 65){
+	//a
+    if (e.keyCode == 65){
 		e.preventDefault();
 		playAudio('keya.wav')
+	}
+    
+    //c
+    if (e.keyCode == 67){
+		e.preventDefault();
+		playAudio('keyc.wav')
+	}
+
+    //d
+    if (e.keyCode == 68){
+		e.preventDefault();
+		playAudio('keyd.wav')
+	}
+
+    //e
+    if (e.keyCode == 69){
+		e.preventDefault();
+		playAudio('keye.wav')
+	}
+
+    //q
+    if (e.keyCode == 81){
+		e.preventDefault();
+		playAudio('keyq.wav')
+	}
+
+    //s
+    if (e.keyCode == 83){
+		e.preventDefault();
+		playAudio('keys.wav')
+	}
+
+    //w
+    if (e.keyCode == 87){
+		e.preventDefault();
+		playAudio('keyw.wav')
+	}
+
+    //x
+    if (e.keyCode == 88){
+		e.preventDefault();
+		playAudio('keyx.wav')
+	}
+
+    //z
+    if (e.keyCode == 90){
+		e.preventDefault();
+		playAudio('keyz.wav')
 	}
 }, false);
