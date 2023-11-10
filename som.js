d8bbceb58102fa5cab70405a64f40ac475d0d3b0
hmArthur <82618569+hmArthur@users.noreply.github.com>
2023-11-10 11:23:35 -0300

    a

--- som.js
+++ som.js
@@ -5,60 +5,64 @@ function playAudio(url, key) {
 	const tecla = document.querySelector("."+key);
 	console.log(key)
 	tecla.style.backgroundColor = "red"
+	
+	setTimeout(function(){ 
+		tecla.style.backgroundColor = "grey"
+	}, 3000);
 }
 
 document.addEventListener("keydown", function(e){
 	//a
     if (e.keyCode == 65){
 		e.preventDefault();
-		playAudio('keya.wav')
+		playAudio('keya.wav', 'keya')
 	}
     
     //c
     if (e.keyCode == 67){
 		e.preventDefault();
-		playAudio('keyc.wav')
+		playAudio('keyc.wav', 'keyc')
 	}
 
     //d
     if (e.keyCode == 68){
 		e.preventDefault();
-		playAudio('keyd.wav')
+		playAudio('keyd.wav', 'keye')
 	}
 
     //e
     if (e.keyCode == 69){
 		e.preventDefault();
-		playAudio('keye.wav')
+		playAudio('keye.wav', 'keye')
 	}
 
     //q
     if (e.keyCode == 81){
 		e.preventDefault();
-		playAudio('keyq.wav')
+		playAudio('keyq.wav', 'keyq')
 	}
 
     //s
     if (e.keyCode == 83){
 		e.preventDefault();
-		playAudio('keys.wav')
+		playAudio('keys.wav', 'keys')
 	}
 
     //w
     if (e.keyCode == 87){
 		e.preventDefault();
-		playAudio('keyw.wav')
+		playAudio('keyw.wav', 'keyw')
 	}
 
     //x
     if (e.keyCode == 88){
 		e.preventDefault();
-		playAudio('keyx.wav')
+		playAudio('keyx.wav', 'keyx')
 	}
 
     //z
     if (e.keyCode == 90){
 		e.preventDefault();
-		playAudio('keyz.wav')
+		playAudio('keyz.wav', 'keyz')
 	}
 }, false);
